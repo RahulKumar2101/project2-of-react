1d80ae54804af30c232b5eadef3e813d17b0223c
RahulKumar2101 <rahulkumarmuz2101@gmail.com>
2024-09-04 01:13:19 +0530

    contact form

---
+++ src/components/navigation.jsx
@@ -0,0 +1,18 @@
+
+const Navigation = () => {
+  return (
+    <nav>
+        <div className="logo">
+            <img src="/src/images/icon.png" alt="icon logo" />
+            <ul>
+                <li>home </li>
+                <li>About </li>
+                <li>Contact </li>
+                
+            </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default Navigation;
